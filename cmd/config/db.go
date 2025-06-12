55dc6409a7324328d82b68df37e203d4a9681cc9
Gustavo Alejandro Agüero Ferrari <95455821+gustyaguero21@users.noreply.github.com>
2025-06-12 13:31:39 -0300

    Update db.go

--- cmd/config/db.go
+++ cmd/config/db.go
@@ -16,7 +16,7 @@ var (
 )
 
 const (
-	CreateTableQuery   = "CREATE TABLE IF NOT EXISTS %s (id VARCHAR(36) UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(36) NOT NULL, last_name VARCHAR(36) NOT NULL, username VARCHAR(36) UNIQUE NOT NULL, email VARCHAR(36) UNIQUE NOT NULL, password VARCHAR(36) NOT NULL)"
+	CreateTableQuery   = "CREATE TABLE IF NOT EXISTS %s (id VARCHAR(36) UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(36) NOT NULL, last_name VARCHAR(36) NOT NULL, username VARCHAR(36) UNIQUE NOT NULL, email VARCHAR(36) UNIQUE NOT NULL, password VARCHAR(100) NOT NULL)"
 	GetByUsernameQuery = "SELECT id,name,last_name,username,email,password FROM %s WHERE username = ?"
 	NewUserQuery       = "INSERT INTO %s (id,name,last_name,username,email,password) VALUES (?,?,?,?,?,?)"
 	DeleteQuery        = "DELETE FROM %s WHERE username = ?"
