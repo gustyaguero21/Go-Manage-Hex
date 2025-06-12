17906691e7cf99bb5a5684841db05f2d211187f8
Gustavo Alejandro Aguero Ferrari <gustavo.aguero.dev@gmail.com>
2025-06-12 15:08:11 -0300

    Commit: adding service

--- internal/app/user/service.go
+++ internal/app/user/service.go
@@ -2,8 +2,13 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	mysqlUser "go-manage-hex/internal/core/user"
+
+	"github.com/google/uuid"
+	"github.com/gustyaguero21/go-core/pkg/encrypter"
+	"github.com/gustyaguero21/go-core/pkg/validator"
 )
 
 type UserServices struct {
@@ -14,6 +19,89 @@ func NewUserService(repo mysqlUser.MysqlRepository) Usecases {
 	return &UserServices{Repo: repo}
 }
 
+func (us *UserServices) SearchUser(ctx context.Context, username string) (search mysqlUser.User, err error) {
+	if !us.Repo.CheckExists(username) {
+		return mysqlUser.User{}, fmt.Errorf("user not found")
+	}
+
+	search, searchErr := us.Repo.GetByUsername(username)
+	if searchErr != nil {
+		return mysqlUser.User{}, searchErr
+	}
+
+	return search, nil
+}
+
 func (us *UserServices) CreateUser(ctx context.Context, user mysqlUser.User) (created mysqlUser.User, err error) {
-	return mysqlUser.User{}, nil
+	if us.Repo.CheckExists(user.Username) {
+		return mysqlUser.User{}, fmt.Errorf("user already exists")
+	}
+
+	uID := uuid.NewString()
+
+	if !validator.ValidateEmail(user.Email) {
+		return mysqlUser.User{}, fmt.Errorf("invalid email address")
+	}
+
+	if !validator.ValidatePassword(user.Password) {
+		return mysqlUser.User{}, fmt.Errorf("invalid password")
+	}
+
+	hash, hashErr := encrypter.PasswordEncrypter(user.Password)
+	if hashErr != nil {
+		return mysqlUser.User{}, hashErr
+	}
+
+	user.ID = uID
+	user.Password = string(hash)
+
+	if createErr := us.Repo.NewUser(user); createErr != nil {
+		return mysqlUser.User{}, fmt.Errorf("error creating user. Error: %s", createErr)
+	}
+
+	return user, nil
+}
+
+func (us *UserServices) DeleteUser(ctx context.Context, username string) error {
+	if !us.Repo.CheckExists(username) {
+		return fmt.Errorf("user not found")
+	}
+
+	if deleteErr := us.Repo.DeleteUser(username); deleteErr != nil {
+		return fmt.Errorf("error deleting user. Error: %s", deleteErr)
+	}
+
+	return nil
+}
+
+func (us *UserServices) UpdateUser(ctx context.Context, username string, user mysqlUser.User) (updated mysqlUser.User, err error) {
+	if !us.Repo.CheckExists(username) {
+		return mysqlUser.User{}, fmt.Errorf("user not found")
+	}
+
+	if !validator.ValidateEmail(user.Email) {
+		return mysqlUser.User{}, fmt.Errorf("invalid email address")
+	}
+
+	if updateErr := us.Repo.UpdateUser(username, user); updateErr != nil {
+		return mysqlUser.User{}, fmt.Errorf("error updating user. Error: %s", updateErr)
+	}
+
+	return user, nil
+}
+
+func (us *UserServices) ChangeUserPwd(ctx context.Context, newPwd, username string) error {
+	if !us.Repo.CheckExists(username) {
+		return fmt.Errorf("user not found")
+	}
+
+	if !validator.ValidatePassword(newPwd) {
+		return fmt.Errorf("invalid password")
+	}
+
+	if changePwd := us.Repo.ChangePwd(newPwd, username); changePwd != nil {
+		return fmt.Errorf("error changing password. Error: %s", changePwd)
+	}
+
+	return nil
 }
--- internal/infrastructure/http/server/urlmapping.go
+++ internal/infrastructure/http/server/urlmapping.go
@@ -1,21 +1,36 @@
 package server
 
 import (
+	"context"
+	"database/sql"
 	"go-manage-hex/cmd/config"
 	"go-manage-hex/internal/infrastructure/db"
 
 	"log"
 	"net/http"
 
+	userservice "go-manage-hex/internal/app/user"
+	entity "go-manage-hex/internal/core/user"
+	repository "go-manage-hex/internal/infrastructure/db/user"
+
 	"github.com/gin-gonic/gin"
 )
 
 func UrlMapping(s *gin.Engine) {
 
-	_, err := db.DatabaseConn()
+	db, err := db.DatabaseConn()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	repo := repository.NewUserMysql(db)
+	repo.CreateTable(config.GetMysqlTable())
+
+	//testSearch(db)
+	//testCreate(db)
+	//testDelete(db)
+	//testUpdate(db)
+	//testChangePwd(db)
 
 	api := s.Group(config.BaseURL)
 
@@ -24,4 +39,85 @@ func UrlMapping(s *gin.Engine) {
 			"message": "pong",
 		})
 	})
+}
+
+func testSearch(db *sql.DB) {
+
+	repo := repository.NewUserMysql(db)
+
+	service := userservice.NewUserService(repo)
+
+	search, searchErr := service.SearchUser(context.Background(), "johndoe")
+	if searchErr != nil {
+		log.Fatal(searchErr)
+	}
+	log.Print(search)
+}
+
+func testCreate(db *sql.DB) {
+	repo := repository.NewUserMysql(db)
+
+	service := userservice.NewUserService(repo)
+
+	user := entity.User{
+		Name:     "John",
+		LastName: "Doe",
+		Username: "johndoe",
+		Email:    "johndoe@example.com",
+		Password: "Password1234567",
+	}
+
+	created, createdErr := service.CreateUser(context.Background(), user)
+	if createdErr != nil {
+		log.Fatal(createdErr)
+	}
+
+	log.Print(created)
+}
+
+func testDelete(db *sql.DB) {
+	repo := repository.NewUserMysql(db)
+
+	service := userservice.NewUserService(repo)
+
+	username := "johndoe"
+
+	if deleteErr := service.DeleteUser(context.Background(), username); deleteErr != nil {
+		log.Fatal(deleteErr)
+	}
+	log.Print("user deleted successfully")
+}
+
+func testUpdate(db *sql.DB) {
+	repo := repository.NewUserMysql(db)
+
+	service := userservice.NewUserService(repo)
+
+	username := "johndoe"
+
+	update := entity.User{
+		Name:     "Johncito",
+		LastName: "Doecito",
+		Email:    "johncitodoecito@example.com",
+	}
+
+	updated, updateErr := service.UpdateUser(context.Background(), username, update)
+	if updateErr != nil {
+		log.Fatal(updateErr)
+	}
+	log.Print(updated)
+}
+
+func testChangePwd(db *sql.DB) {
+	repo := repository.NewUserMysql(db)
+
+	service := userservice.NewUserService(repo)
+
+	username := "johndoe"
+	newPwd := "NewPassword1234"
+
+	if err := service.ChangeUserPwd(context.Background(), newPwd, username); err != nil {
+		log.Fatal(err)
+	}
+	log.Print("password changed successfully")
 }
